8690548d7248db1a962d2ec92f61f529f5f7bdbc
Hreshikesh <ailhreshikesh@gmail.com>
2025-11-20 17:58:28 +0530

    Cinexa Spring boot project

--- cinexa/src/main/java/com/xworkz/cinexa/restcontroller/AdminRestController.java
+++ cinexa/src/main/java/com/xworkz/cinexa/restcontroller/AdminRestController.java
@@ -1,6 +1,8 @@
 package com.xworkz.cinexa.restcontroller;
 
 import com.xworkz.cinexa.service.AdminService;
+import com.xworkz.cinexa.service.EmailService;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping("/api/Admin")
+@CrossOrigin("http://localhost:3000/")
 public class AdminRestController {
 
 
     private final AdminService adminService;
 
-    public AdminRestController(AdminService adminService) {
+    private final EmailService emailService;
+
+    public AdminRestController(AdminService adminService, EmailService emailService) {
         this.adminService = adminService;
+        this.emailService = emailService;
     }
 
     @PostMapping("/sentOtp/{adminEmail}")
-    public ResponseEntity<String> verifyAndSendOtp(@PathVariable String adminEmail, HttpSession httpSession){
+    public ResponseEntity<String> verifyAndSendOtp(@PathVariable String adminEmail, HttpSession httpSession) throws MessagingException {
 
        String result= adminService.findByEmail(adminEmail);
        if(result.equals("Notfound")){
@@ -27,6 +33,8 @@ public class AdminRestController {
        }else{
            httpSession.setAttribute("adminEmail",adminEmail);
            String otp=adminService.generateOtp(adminEmail);
+           log.info(otp);
+           emailService.sendAdminOtpMail(adminEmail,otp);
             return ResponseEntity.ok("OTP Sent to Your Registered Mail");
        }
 
--- cinexa/src/main/java/com/xworkz/cinexa/service/EmailService.java
+++ cinexa/src/main/java/com/xworkz/cinexa/service/EmailService.java
@@ -1,14 +1,38 @@
 package com.xworkz.cinexa.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
 
+    public EmailService(JavaMailSender javaMailSender, SpringTemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    @Async
+    public  void sendAdminOtpMail(String email,String otp) throws MessagingException {
+        Context context=new Context();
+        context.setVariable("otp",otp);
+        String html= templateEngine.process("AdminOtpTemplate",context);
+        System.out.println(html);
+        MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "UTF-8");
+            helper.setTo(email);
+            helper.setSubject("Your OTP for Admin Login");
+            helper.setText(html, true);
+            javaMailSender.send(message);
+    }
 
 }
