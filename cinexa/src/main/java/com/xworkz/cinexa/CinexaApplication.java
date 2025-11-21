eded434aca21ed23cd3bd2f92aedcb9f29c1a001
Hreshikesh <ailhreshikesh@gmail.com>
2025-11-21 12:20:21 +0530

    Cinexa Spring boot project

---
+++ cinexa/src/main/java/com/xworkz/cinexa/CinexaApplication.java
@@ -0,0 +1,13 @@
+package com.xworkz.cinexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinexaApplication.class, args);
+	}
+
+}
---
+++ cinexa/src/test/java/com/xworkz/cinexa/CinexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xworkz.cinexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
