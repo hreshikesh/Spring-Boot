11ab61f0a4fe03605b291c81397e569e1c86b792
Hreshikesh <ailhreshikesh@gmail.com>
2025-12-01 23:03:34 +0530

    Spring boot project

---
+++ cinexa/src/main/java/com/xworkz/cinexa/service/MovieLangauagesService.java
@@ -0,0 +1,10 @@
+package com.xworkz.cinexa.service;
+
+
+import com.xworkz.cinexa.dto.MovieLangauageDto;
+
+import java.util.List;
+
+public interface MovieLangauagesService {
+    List<MovieLangauageDto> getAllMovieLangauage();
+}
---
+++ cinexa/src/main/java/com/xworkz/cinexa/service/MovieLangaugeServiceImpl.java
@@ -0,0 +1,31 @@
+package com.xworkz.cinexa.service;
+
+import com.xworkz.cinexa.dto.MovieLangauageDto;
+import com.xworkz.cinexa.repository.MovieLanguageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class MovieLangaugeServiceImpl implements MovieLangauagesService {
+
+    private final MovieLanguageRepository movieLanguageRepository;
+
+    public MovieLangaugeServiceImpl(MovieLanguageRepository movieLanguageRepository) {
+        this.movieLanguageRepository = movieLanguageRepository;
+    }
+
+    @Override
+    public List<MovieLangauageDto> getAllMovieLangauage() {
+        List<MovieLangauageDto> movieLangauageDtos=new ArrayList<>();
+        movieLanguageRepository.findAll().forEach(
+                data->{
+                        MovieLangauageDto movieLangauageDto=new MovieLangauageDto();
+                    BeanUtils.copyProperties(data,movieLangauageDto);
+                    movieLangauageDtos.add(movieLangauageDto);
+                }
+        );
+        return movieLangauageDtos;
+    }
+}
