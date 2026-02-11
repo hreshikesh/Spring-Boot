44f1b29479394324d3b48d0fb7397fe14335b2bc
Hreshikesh <ailhreshikesh@gmail.com>
2026-02-11 10:29:09 +0530

    Food API

--- foods/src/main/java/com/xworkz/foods/dto/UserDto.java
+++ foods/src/main/java/com/xworkz/foods/dto/UserDto.java
@@ -10,4 +10,11 @@ import lombok.NoArgsConstructor;
 public class UserDto {
     private String userName;
     private String userEmail;
+    private long userPhone;
+    private String userPassword;
+    private int userAge;
+    private String userGender;
+    private String userCity;
+    private String userCountry;
+    private String userSkills;
 }
