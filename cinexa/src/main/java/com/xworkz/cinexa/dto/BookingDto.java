8659b617ff653e8f2bf90b3db41584a8c28b0c51
Hreshikesh <ailhreshikesh@gmail.com>
2026-02-25 13:43:30 +0530

    Spring boot project

--- cinexa/src/main/java/com/xworkz/cinexa/dto/BookingDto.java
+++ cinexa/src/main/java/com/xworkz/cinexa/dto/BookingDto.java
@@ -3,12 +3,13 @@ package com.xworkz.cinexa.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class BookingDto {
+public class BookingDto implements Serializable {
     private int movieId;
     private String userEmail;
     private int price;
