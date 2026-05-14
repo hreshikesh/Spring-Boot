061a6aabc1f1defb8e7d8dc894e06577a39fa12a
Hreshikesh <ailhreshikesh@gmail.com>
2026-05-14 20:04:10 +0530

    Food API

---
+++ foods/src/main/java/com/xworkz/foods/restcontroller/StudentController.java
@@ -0,0 +1,36 @@
+package com.xworkz.foods.restcontroller;
+
+
+import com.xworkz.foods.dto.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+
+    @PostMapping("/result")
+    public String getStudentResult(
+            @RequestBody Student student) {
+
+        return studentService.getResult(student);
+    }
+
+    @GetMapping("/add")
+    public int add(
+            @RequestParam int a,
+            @RequestParam int b) {
+
+        return studentService.addNumbers(a, b);
+    }
+
+    @PostMapping("/scholarship")
+    public boolean scholarship(
+            @RequestBody Student student) {
+
+        return studentService
+                .isEligibleForScholarship(student);
+    }
+}
